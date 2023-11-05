cd7bcd4bb131d69ff4fc91ad5f15d9e4c717de81
lninyow <lidennino.sagmon@cit.edu>
2023-11-05 22:13:39 +0800

    finish 03 exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function(word) {
 
+    let newStr = ''
+
+    for(let i = word.length - 1; i >= 0; i--) {
+        newStr += word[i]
+    }
+
+    return newStr;
 };
 
 // Do not edit below this line
